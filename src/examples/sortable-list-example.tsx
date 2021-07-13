a256b2f03dc75572ef1573d9e60caac2b63f2d0b
Martin Pengelly-Phillips <dev@thisbeyond.com>
2021-07-13 23:27:23 +0100

    Add sortable debugging information

--- src/examples/sortable-list-example.tsx
+++ src/examples/sortable-list-example.tsx
@@ -5,6 +5,8 @@ import {
   SortableContext,
   createSortable,
   closestLayoutCenter,
+  useDragDropContext,
+  transformLayout,
 } from "@thisbeyond/solid-dnd";
 import { createSignal, For } from "solid-js";
 
@@ -55,6 +57,73 @@ export const SortableListExample = () => {
       <DragOverlay>
         <div class="sortable">{`Sortable ${activeItem()}`}</div>
       </DragOverlay>
+      <Debug />
     </DragDropContext>
+  );
+};
+
+const Debug = () => {
+  const [state, { activeDraggable }] = useDragDropContext();
+
+  const droppableLayouts = () => {
+    const layouts = [];
+    const transformedLayouts = [];
+    for (const droppable of Object.values(state.droppables)) {
+      layouts.push(droppable.layout);
+      transformedLayouts.push(
+        transformLayout({
+          layout: droppable.layout,
+          transform: droppable.transform,
+        })
+      );
+    }
+    const draggable = activeDraggable();
+    let draggableLayout = null;
+    if (draggable) {
+      draggableLayout = transformLayout({
+        layout: draggable.layout,
+        transform: draggable.transform,
+      });
+    }
+
+    return {
+      layouts,
+      transformedLayouts,
+      draggableLayout,
+    };
+  };
+
+  return (
+    <div class="absolute top-0 left-0 w-full h-full pointer-events-none">
+      <For each={droppableLayouts().transformedLayouts}>
+        {(layout, index) => {
+          const style = {
+            position: "absolute",
+            top: Math.round(layout.top) + "px",
+            left: Math.round(layout.left) + "px",
+            width: Math.round(layout.width) + "px",
+            height: Math.round(layout.height) + "px",
+          };
+          return (
+            <div class="border-2 border-red-500" style={style}>
+              {index()}
+            </div>
+          );
+        }}
+      </For>
+      {droppableLayouts().draggableLayout ? (
+        <div
+          class="border-2 border-blue-500"
+          style={{
+            position: "absolute",
+            top: Math.round(droppableLayouts().draggableLayout.top) + "px",
+            left: Math.round(droppableLayouts().draggableLayout.left) + "px",
+            width: Math.round(droppableLayouts().draggableLayout.width) + "px",
+            height:
+              Math.round(droppableLayouts().draggableLayout.height) + "px",
+          }}
+        />
+      ) : null}
+    </div>
   );
 };
