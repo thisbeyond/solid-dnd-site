472ffb9e3216f41ba9b7ec2ecdcc1b26c268571a
Martin Pengelly-Phillips <dev@thisbeyond.com>
2024-07-23 16:28:24 +0100

    Update dependencies and build approach.

--- src/site.tsx
+++ src/site.tsx
@@ -7,17 +7,17 @@ import "@fontsource/poppins/900.css";
 import "@fontsource/jetbrains-mono/400.css";
 
 import { Component, lazy } from "solid-js";
-import { Route, Routes } from "@solidjs/router";
+import { Route } from "@solidjs/router";
 
 import Home from "./pages/home";
 const NotFoundPage = lazy(() => import("./errors/404"));
 
 const Site: Component = () => {
   return (
-    <Routes>
+    <>
       <Route path="/" component={Home}></Route>
       <Route path="/*all" component={NotFoundPage}></Route>
-    </Routes>
+    </>
   );
 };
 
